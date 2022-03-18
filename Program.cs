c35e69828fdda4169d2bab1f5efd94e15c5d71f7
enkhbatx <92764987+enkhbatx@users.noreply.github.com>
2022-03-18 02:10:01 -0400

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,69 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+
+// Introduction to C# and data Types
+// UNderstanding data types 
+/* 1. int
+ * 2. double 
+ * 3. int
+ * 4. string
+ * 5. decimal
+ * 6. float
+ * 7. float
+ * 8. decimal
+ * 9. double 
+ * 10. int
+ * 11. int
+ * 12. int
+ * 
+ */
+
+
+/* 2. Value types will directly hold the value, while reference types will hold memory address or reference for the value 
+ * boxing: is converting value type to reference tpye
+ * unboxing is converting reference type to value type
+ * 
+ * 
+ */
+
+
+/* 
+ * 3. Managed resources are those that are pure . NET code and managed by the runtime and are under its direct control. 
+ * Unmanaged resources are those that are not.
+ * 
+ */
+
+
+/* 4. Garbage collector allocates memory of the application 
+ */
+
+
+/* Control Flow and converting types
+ * 1. there will be build error 
+ * 2. build error
+ * 3. You get an error
+ * 4. y++ first execute then increment the value. ++y increments value then executes the statement 
+ * 5. break - ends the loop. continue - starts the next iteration. return - is used to return a value from a function; I have never used return in a loop. 
+ * 6. the initialization, the condition, and the advancement to the next iteration. The initialization and the condition are required.
+ * 7. = assigns value to a variable. == campares values and return boolena value. 
+ * 8.no
+ * 9. _ replaces the default keyword to signify that it should match anythign if reached
+ * 10. IEnumerable permits enumeration by using a foreach loop.
+ * 11.
+ * 
+ */
+
+
+/* Arrays and Strings 
+ * test knowledge 
+ * 1. if sting will remain constant then we cna use String, if not we use StringBuilder.
+ * 2.The Array class is the base class for all arrays 
+ * 3. using Array.Sort() method
+ * 4.Length of the array 
+ * 5. We cannot store multiple data types to array.
+ * 6. CopyTo() copies array in to another existing array. Clone() creates a new array (a copy).
+ * 7.
+ * 
+ * 
+ */
