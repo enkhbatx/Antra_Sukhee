e0db8b059730ba44ea2333952a5d9e3990c70678
enkhbatx <92764987+enkhbatx@users.noreply.github.com>
2022-03-18 22:06:51 -0400

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,98 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
+
+
+// 03 Object-Oriented Programming
+
+/* 1. the six different types of access modifiers are 
+ *  - Public : makes object accessible from everywhere in project
+ *  - Private : object accessible inside a class.
+ *  - Protected : onject is accessible inside the class nad in all classes that derive from the class 
+ *  - Internal : object access is limited excusively to classes defined withing the current project assembly 
+ *  - Protected Interal : we can only access the object in same project assembly or in a derived class in other assemblies
+ *  - Private Protected : we can access members inside the containing class or in a class that derives from a containing class, but only in the same project assembly.  
+ */
+
+
+/* 2. const is initialized at compile time, 
+ * - readonly keyword allow the variable to be initialized either at compile time or runtime
+ *  
+ * 
+ */
+
+/*  3. constructor initialized a newly created object of class. 
+ * 
+ */
+
+
+/*  4. Partial keywords indicate that other parts of the class can be defined in the namespace. 
+ *  - this allows multiple developers to work simultaneously in the same calss in defferent files. 
+ *  
+ */
+
+/*  5. Tuple is a data structure that contains a sequence of elements of different data types.
+ * 
+ */
+
+
+/* 6. C# record keyword is used to define a reference type that provides built-in functionality for encapsulating data
+ * 
+ */
+
+/* 7. Overloading occurs when two or more methods in one class have the same method name 
+ *   but different parameters.
+ *   - Overriding occurs when two methods have the same method name and parameters. 
+ * 
+ */
+
+
+/* 8. Field is a variable of any type that is declared directly in a class.
+ *  - Property is a member that provides a flexible mechanism to read, write or compute the value of a private field.
+ * 
+ */
+
+
+/* 9. We can make a parameter optional by assigning default for that parameter. 
+ * 
+ */
+
+
+/* 10. Abstract class allows us to make functions taht sublclasses can implement or override.
+ *   - interface only permits us to state functionality but not to implement it.
+ * 
+ */
+
+
+/* 11. Public 
+ * 
+ */
+
+// 12. True
+
+// 13. True 
+
+// 14. True
+
+// 15. False
+
+// 16. True 
+
+// 17. True  --> over ride only virtual method
+
+// 18. False 
+
+// 19. False
+
+// 20. False 
+
+// 21. True 
+
+// 22. True 
+
+// 23. True 
+
+
+
+
+
+
